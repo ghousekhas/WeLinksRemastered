21dd45e5ed45d21ba78838a22487013f24687a0d
Anam <anamxali1@gmail.com>
2020-11-05 08:37:18 +0530

    Vendors images

--- src/components/Vendor.js
+++ src/components/Vendor.js
@@ -1,128 +1,131 @@
 import React, { useState } from 'react';
-import {View, StyleSheet, Text, Dimensions,Image} from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, StyleSheet, Text, Dimensions, Image } from 'react-native';
 import Button from './Button';
 import Stars from './Stars';
-import { color } from 'react-native-reanimated';
 
 
 
-const Vendor = ({name,brands,stars,reviews,onSelected,picture,buttonVisible,address,scrap,imageUrl}) => {
-    const [imageHeight,setImageHeight]=useState(0);
+const Vendor = ({ name, brands, stars, reviews, onSelected, buttonVisible, address, scrap, imageUrl }) => {
+    const [imageHeight, setImageHeight] = useState(0);
 
-    const renderButton=()=>{
-        if(buttonVisible!= false){
-            return(
-                <View style={{...style.button,flex: 1,alignSelf: 'center',justifyContent: 'center'}}>
-                <Button text='Select' onTouch={onSelected} />
-            </View>
+    const renderButton = () => {
+        if (buttonVisible != false) {
+            return (
+                <View style={style.button}>
+                    <Button text='Select' onTouch={onSelected} />
+                </View>
             );
         }
     };
 
-    const renderDesc= ()=>{
-        if(scrap !=null)
-        return(
-            <View style = {{flexDirection: 'row',width: '60%',alignSelf: 'center'}}>
-                <Text style={style.brandsTitle}>{scrap}</Text>
-            </View>);
-        if(buttonVisible != false)
-            return(
-            <View style = {{flexDirection: 'row'}}>
-                <Text style={style.brandsTitle}>Brands: </Text>
-                <Text style={style.brands}>{brands}</Text>
-            </View>
+    const renderDesc = () => {
+        if (scrap != null)
+            return (
+                <View style={{ flexDirection: 'row', width: '60%', alignSelf: 'center' }}>
+                    <Text style={style.brandsTitle}>{scrap}</Text>
+                </View>);
+        if (buttonVisible != false)
+            return (
+                <View style={{ flexDirection: 'row' }}>
+                    <Text style={style.brandsTitle}>Brands: </Text>
+                    <Text style={style.brands}>{brands}</Text>
+                </View>
             );
-        
 
-        return(
-            <View style = {{flexDirection: 'row',width: '60%',alignSelf: 'center'}}>
+
+        return (
+            <View style={{ flexDirection: 'row', width: '60%', alignSelf: 'center' }}>
                 <Text style={style.brands}>{address}</Text>
             </View>
         );
     };
 
-    return(
-    <View onLayout={({nativeEvent})=>{
-        console.log('nativeevent',nativeEvent)
-        if(nativeEvent.layout.height!=null)
-        setImageHeight(nativeEvent.layout.height);
+    return (
+        <View style={style.container}>
+            <Image style={{ ...style.image, height: imageHeight, aspectRatio: 1 / 1.7, alignSelf: 'flex-end', borderWidth: 1, flex: 1 }} source={{ uri: imageUrl }} />
 
-    }} style={style.container}>
-    <View style={{marginStart: '4%'}}>
-        
-        <View style={style.top}>
-        <View style = {{flexDirection: 'row',justifyContent: 'center',alignItems: 'center'}}>
-            <View style={{flexDirection: 'column',flex:2,paddingLeft: 15,marginTop: 20}}>
-            <Text style={style.name}> {name}</Text>
-            {renderDesc()}
-            <View style = {{flexDirection: 'row',marginStart: '22%'}}>
-        <Stars number={stars}/>
-            <Text style = {style.review}>({reviews} reviews)</Text>
-        </View>
-        </View>
-        {renderButton()}
-        </View>
-           
-        </View>
+            <View style={{ marginStart: '4%', flex: 4 }}>
 
-        
+                <View onLayout={({ nativeEvent }) => {
+                    //  console.log('nativeevent',nativeEvent)
+                    //   if(nativeEvent.layout.height!=null)
+                    setImageHeight(nativeEvent.layout.height);
 
-       
-        </View>
-        <Image style={{...style.image,height: imageHeight*0.75,width: imageHeight*0.75,alignSelf: 'center'}} source={{uri: imageUrl}}/>
-    </View>)
+                }} style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+                    <View style={{ flexDirection: 'column', flex: 2, paddingLeft: 15, marginTop: 20 }}>
+                        <Text style={style.name}> {name}</Text>
+                        {renderDesc()}
+                        <View style={{ flexDirection: 'row' }}>
+                            <Stars number={stars} />
+                            <Text style={style.review}>({reviews} reviews)</Text>
+                        </View>
+                    </View>
+                    {renderButton()}
+                </View>
+
+
+
+
+
+            </View>
+        </View>)
 };
 
 const style = StyleSheet.create({
-    container:{
-        margin: '1%',
+    container: {
+        margin: '0.5%',
         marginBottom: '10%',
         flexDirection: 'row',
-        backgroundColor: 'transparent'
-        
-    },
-    top:{
-        flexDirection: 'column',
-        width: Dimensions.get('window').width-15,
-        marginStart: '1%',      
-        alignItems: 'flex-end',
-        
-        
+        backgroundColor: 'transparent',
+        padding: '1%',
+        borderWidth: 5,
+        borderColor: 'transparent'
+        //  paddingVertical: 10
 
     },
-    button : {
+    top: {
         flexDirection: 'column',
-        width: Dimensions.get('window').width-15,
+        //   width: Dimensions.get('window').width-15,
+        marginStart: '1%',
+        alignItems: 'flex-end',
+
+
+
+    },
+    button: {
+        flexDirection: 'column',
+        width: Dimensions.get('window').width - 20,
+        alignSelf: 'center', flex: 1,
         alignSelf: 'center',
+        justifyContent: 'center'
     }
     ,
-    
+
     name: {
         fontWeight: 'bold',
-        marginStart: '20%',
+
         fontSize: 17,
-        alignSelf: 'flex-start',
+        //  alignSelf: 'flex-start',
         color: 'black'
     },
-    brandsTitle:{
+    brandsTitle: {
         color: 'gray',
-        marginStart: '22%',
+
         marginTop: '2%',
         fontWeight: 'bold',
-       
-    },
-    
-    brands:{
-        color: 'gray',
-        flex: 1,
-        
-        marginTop: '2%',
-        
-     
 
     },
-    review:{
+
+    brands: {
+        color: 'gray',
+        flex: 1,
+
+        marginTop: '2%',
+
+
+
+    },
+    review: {
         color: 'gray',
         marginStart: '4%',
         marginTop: '2%',
@@ -131,13 +134,13 @@ const style = StyleSheet.create({
     image: {
         width: 70,
         height: 90,
-        position: 'absolute',
-        margin :'1%'
-        
-       
+        //  position: 'absolute',
+        //  margin :'1%'
+
+
     }
-    
-    
+
+
 });
 
 
--- src/screens/PaperVendors.js
+++ src/screens/PaperVendors.js
@@ -1,21 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 
-import {View, StyleSheet, Text, Dimensions,Image,BackHandler} from 'react-native';
+import { View, StyleSheet, Text, Dimensions, Image, BackHandler } from 'react-native';
 import { TouchableOpacity, FlatList } from 'react-native-gesture-handler';
 import Vendor from '../components/Vendor';
 import AppBar from '../components/AppBar';
 import { Feather } from '@expo/vector-icons';
-import {Colors,Styles} from '../Constants';
+import { Colors, Styles } from '../Constants';
 import Axios from 'axios';
 import qs from 'qs';
-import {Config} from  '../Constants';
+import { Config } from '../Constants';
 
 const PaperVendors = (props) => {
-    const address= props.route.params.address;
-    const [offset,setOffset] = useState(0);
-    const {tag} = props.route.params;
-    const {actualUser}=props.route.params;
+    const address = props.route.params.address;
+    const [offset, setOffset] = useState(0);
+    const { tag } = props.route.params;
+    const [imageHeight, setImageHeight] = useState(0);
+
+    const { actualUser } = props.route.params;
     const words = {
         paper: 'Newspaper vendors in your locality',
 
@@ -25,125 +27,125 @@ const PaperVendors = (props) => {
 
 
 
-    const [vendors,updateVendors] = useState([]);
+    const [vendors, updateVendors] = useState([]);
 
-    const retrieveData= async (t)=>{
-        if(t<=0)
+    const retrieveData = async (t) => {
+        if (t <= 0)
             return;
-        Axios.get(Config.api_url+'php?action=getVendors&'+qs.stringify({
+        Axios.get(Config.api_url + 'php?action=getVendors&' + qs.stringify({
             vendor_type: 'newspaper',
             lat: address.lat,
             lng: address.lng
-        }),).then((response)=>{
-            try{
-                console.log('LOGGGGSSSS',address.lat);
+        })).then((response) => {
+            try {
+                console.log('LOGGGGSSSS', address.lat);
                 updateVendors(response.data.vendor);
             }
-            catch(error){
-                console.log('milkvendoreasarror',error);
+            catch (error) {
+                console.log('milkvendoreasarror', error);
                 //retrieveData(t-1);
             }
-        },(error)=>{
-            console.log('milkvendorerror',error);
+        }, (error) => {
+            console.log('milkvendorerror', error);
             //retrieveData(t-1);
         })
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         retrieveData(10);
-    },[]);
+    }, []);
     useFocusEffect(
         React.useCallback(() => {
-          const onBackPress = () => {
-     //     console.log('Go to homescreen');
-           props.navigation.navigate('AddressList');
-              return true;
-            
-          };
-    
-          BackHandler.addEventListener('hardwareBackPress', onBackPress);
-    
-          return () =>
-            BackHandler.removeEventListener('hardwareBackPress', onBackPress);
-        },)
-      );
-    return(
-    <View style={{flex: 1,backgroundColor: 'white'}}>
+            const onBackPress = () => {
+                //     console.log('Go to homescreen');
+                props.navigation.navigate('AddressList');
+                return true;
 
-<AppBar back funct={() => {
-        props.navigation.pop();
-        }} />
+            };
 
-        <View onLayout={({nativeEvent}) => {
-                setOffset(nativeEvent.layout.height);
-        }} style={{flexDirection: 'row',marginTop: Dimensions.get('window').height/14}}>
-        <Image  style ={{...style.avatar}} source={ actualUser.img_url.trim()  != ''? {uri: actualUser.img_url}: require('../../assets/notmaleavatar.png')  }/>
-      
-       
-        <View style={style.header}>
-        <Text style ={{...style.username}}>{actualUser.name}</Text>
-        <View style ={{...style.address}}>
-        <View style = {{flexDirection: 'row',alignItems: 'center'}}>
-        <Feather name="map-pin" size={12} color="black" />
-        <Text style={{fontSize: 13}}>{ " " +address.addr_name}</Text>
-        </View>
-       
-        <Text style={{fontSize: 13}}>{address.addr_details+".\nLandmark: " + address.addr_landmark }</Text>
-        </View>
-    </View>
-        </View>
-        <View style={Styles.grayfullline} />
-    
-        <View style={style.heading}>
-<Text style={{...Styles.title,fontSize: 17}}>{words.paper}</Text>
-</View>
-    
-        <FlatList 
-            data={vendors}
-            keyExtractor={(item) => item.name}
-            renderItem={({item}) => {
-                const actualUser=props.route.params.actualUser;
-                const vendorName = item.name;
-                const vendorStars = item.avg_ratings;
-                const vendorReviews = item.reviews_number;
-                var brandsString= '';
-                const brands= item.brands!=undefined? item.brands: [];
-                const imageUrl=item.vendor_img_url;
-                const vendorId=item.vendor_id;
-                const vendorAddress= item.addresses[0]!= undefined? item.addresses[0].addr_details+' '+item.addresses[0].addr_landmark+' '+item.addresses[0].addr_pincode: ' ';
-              //  console.log('itembrands',brands);
-                for(let i=0;i<brands.length-1;i++)
-                    brandsString=brandsString+brands[i].brand.toString()+','+' ';
-                if(brands.length>0)
-                    brandsString=brandsString+brands[brands.length-1].brand.toString();
-    
-                return(
-                    <Vendor name={item.name} brands={brandsString} stars={item.avg_ratings} reviews={item.reviews_number} imageUrl={imageUrl}
-                    onSelected={() => {
-                 
-                    props.navigation.navigate('VendorScreen1',{
-                        tag: "Paper",
-                        name: vendorName,
-                        stars: vendorStars,
-                        reviews: vendorReviews,
-                        address: address,
-                        vendorAddress: vendorAddress,
-                        imageUrl: imageUrl,
-                        actualUser: actualUser,
-                        vendorId: vendorId
-                    })
-                    
-                        
-                    }}
-    
-                    />
-                )
-    
-            }}
-        />
-    
-       
-    
+            BackHandler.addEventListener('hardwareBackPress', onBackPress);
+
+            return () =>
+                BackHandler.removeEventListener('hardwareBackPress', onBackPress);
+        })
+    );
+    return (
+        <View style={{ flex: 1, backgroundColor: 'white' }}>
+
+            <AppBar back funct={() => {
+                props.navigation.pop();
+            }} />
+
+            <View onLayout={({ nativeEvent }) => {
+                setImageHeight(0.75 * nativeEvent.layout.height)
+            }} style={{ flexDirection: 'row', marginTop: Dimensions.get('window').height / 14 }}>
+                <Image style={{ ...style.avatar, height: imageHeight, aspectRatio: 1 / 1.8, flex: 1 }} source={actualUser.img_url.trim() != '' ? { uri: actualUser.img_url } : require('../../assets/notmaleavatar.png')} />
+
+
+                <View style={{ ...style.header, flex: 5 }}>
+                    <Text style={{ ...style.username }}>{actualUser.name}</Text>
+                    <View style={{ ...style.address }}>
+                        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                            <Feather name="map-pin" size={12} color="black" />
+                            <Text style={{ fontSize: 13 }}>{" " + address.addr_name}</Text>
+                        </View>
+
+                        <Text style={{ fontSize: 13 }}>{address.addr_details + ".\nLandmark: " + address.addr_landmark}</Text>
+                    </View>
+                </View>
+            </View>
+            <View style={Styles.grayfullline} />
+
+            <View style={style.heading}>
+                <Text style={{ ...Styles.title, fontSize: 17 }}>{words.paper}</Text>
+            </View>
+
+            <FlatList
+                data={vendors}
+                keyExtractor={(item) => item.name}
+                renderItem={({ item }) => {
+                    const actualUser = props.route.params.actualUser;
+                    const vendorName = item.name;
+                    const vendorStars = item.avg_ratings;
+                    const vendorReviews = item.reviews_number;
+                    var brandsString = '';
+                    const brands = item.brands != undefined ? item.brands : [];
+                    const imageUrl = item.vendor_img_url;
+                    const vendorId = item.vendor_id;
+                    const vendorAddress = item.addresses[0] != undefined ? item.addresses[0].addr_details + ' ' + item.addresses[0].addr_landmark + ' ' + item.addresses[0].addr_pincode : ' ';
+                    //  console.log('itembrands',brands);
+                    for (let i = 0; i < brands.length - 1; i++)
+                        brandsString = brandsString + brands[i].brand.toString() + ',' + ' ';
+                    if (brands.length > 0)
+                        brandsString = brandsString + brands[brands.length - 1].brand.toString();
+
+                    return (
+                        <Vendor name={item.name} brands={brandsString} stars={item.avg_ratings} reviews={item.reviews_number} imageUrl={imageUrl}
+                            onSelected={() => {
+
+                                props.navigation.navigate('VendorScreen1', {
+                                    tag: "Paper",
+                                    name: vendorName,
+                                    stars: vendorStars,
+                                    reviews: vendorReviews,
+                                    address: address,
+                                    vendorAddress: vendorAddress,
+                                    imageUrl: imageUrl,
+                                    actualUser: actualUser,
+                                    vendorId: vendorId
+                                })
+
+
+                            }}
+
+                        />
+                    )
+
+                }}
+            />
+
+
+
         </View>)
 }
 
@@ -151,50 +153,48 @@ const style = StyleSheet.create({
     header: {
         margin: '5%',
         padding: '3%',
-        marginStart: '20%'
-        
+
+
     },
     username: {
         fontWeight: 'bold',
-        marginStart: 50,
+
         fontSize: 18,
-      
+
         color: 'black'
     },
     address: {
         marginTop: '3%',
         borderRadius: 5,
         backgroundColor: Colors.whiteBackground,
-       borderColor: Colors.seperatorGray,
-       borderWidth: 0.5,
-        marginStart:48,
+        borderColor: Colors.seperatorGray,
+        borderWidth: 0.5,
+
         paddingHorizontal: 10,
         paddingVertical: 5,
         fontSize: 13,
         elevation: 1
-        
+
     },
-    line:{
+    line: {
         borderWidth: 0.5,
         borderColor: 'gray',
         marginVertical: '2%',
-          
-        
+
+
     },
     heading: {
-       
+
         marginBottom: '5%',
-        
+
     },
     avatar: {
-        width: 50,
-        height: 50,
-        margin: '5%',
+        margin: '3%',
         padding: '3%',
-        position: 'absolute',
-     
-        
-       
+        alignSelf: 'center'
+
+
+
     }
 
 })
