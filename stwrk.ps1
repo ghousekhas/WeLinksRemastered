95ccf0d6cf353dc4ae734018a299d87488cee059
ghousekhas <49071980+ghousekhas@users.noreply.github.com>
2020-11-26 20:19:41 +0530

    startwork

---
+++ stwrk.ps1
@@ -0,0 +1,8 @@
+param ($one);
+if($one.Length -ne 0){
+    adb connect 192.168.0.10${one}:5555;
+    #Write-Output 192.168.0.${one}:5555;
+}
+adb wait-for-device;
+adb reverse tcp:8081 tcp:8081;
+npx react-native start;
