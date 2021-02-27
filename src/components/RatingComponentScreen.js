bebae65d6017b40e073cbbf4b57b825c2f0f5d78
ghousekhas <49071980+ghousekhas@users.noreply.github.com>
2021-02-27 15:05:06 +0530

    Corporate scrap ratings

--- src/components/RatingComponentScreen.js
+++ src/components/RatingComponentScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, StyleSheet, Text,TextInput} from 'react-native';
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import {Styles,strings_screen,dimen,Colors} from '../Constants';
