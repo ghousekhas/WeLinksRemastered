08af2e980835f971d7efad57ad763e8188d6104a
Ghouse Khasim <49071980+ghousekhas@users.noreply.github.com>
2021-04-24 02:57:41 -0700

    authservice

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -25,6 +25,7 @@ export const AuthContext = React.createContext({
 })
 
 export default function AuthProvider({ children }) {
+  const [user, setUser] = useState(AuthConstants.loading)
   const [vendor, setVendor] = useState(AuthConstants.loading)
   const debug = true
   const debugNumber = '9535311386'
